dd32c96ad7f4b31a4a2dff154f358f2d207e57a9
Giacomo Rebonato <grebonato@outlook.ie>
2025-12-01 16:47:53 +0100

    bundle lexical

--- build.ts
+++ build.ts
@@ -1,13 +1,13 @@
 await Bun.build({
   entrypoints: ['src/index.ts'],
   outdir: './dist',
+  target: 'browser',
+  format: 'esm',
   external: [
     'react',
     'react-dom',
     'react/jsx-runtime',
     'react/jsx-dev-runtime',
-    'lexical',
-    '@lexical/*',
   ],
 })
 
